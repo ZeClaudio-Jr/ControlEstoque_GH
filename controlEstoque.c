dd4eb4018efc630d36ddf4d0cfd7e28ad5247d27
ZeClaudio-Jr <zeclaudio_jr5@yahoo.com.br>
2021-01-23 15:54:42 -0300

    Adicionando arq.c

---
+++ controlEstoque.c
@@ -0,0 +1,49 @@
+///////////////////////////////////////////////////////////////////////////////
+///             UFRN - Universidade Federal do Rio Grande do Norte          ///
+///                 Centro de Ensino Superior do Seridó - Caicó             ///
+///               Departamento de Computação e Tecnologia                   ///
+///                  Disciplina DCT1106 * Programação                       ///
+///              SIG-Inventory: Sistema de Controle de Estoque              ///
+///         Developed by: Annielly Ferreira de Sousa - Jan, 2021, and       ///
+///                       José Cláudio de Araújo Júnior - Jan, 2021         ///
+///////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void menuSobre(void);
+
+int main(void) {
+    menuSobre();
+    return 0;
+
+}
+
+
+void menuSobre(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///          UFRN - Universidade Federal do Rio Grande do Norte             ///\n");
+    printf("///             Centro de Ensino Superior do Seridó - Caicó                 ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                   Disciplina DCT1106 * Programação                      ///\n");
+    printf("///             SIG-Inventory: Sistema de Controle de Estoque               ///\n");
+    printf("///       Developed by: Annielly Ferreira de Sousa - Jan, 2021, and         ///\n");
+    printf("///                     José Cláudio de Araújo Júnior - Jan, 2021           ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///        = = SIG-Inventory: Sistema de Controle de Estoque = =            ///\n");
+    printf("///                                                                         ///\n");
+    printf("///             Programa desenvolvido em prol do desenvolvimento e          ///\n");
+    printf("///            aperfeiçoamento da disciplina DCT1106 - Programação.         ///\n");
+    printf("///          Tal trabalho pretende instituir um programa de controle        ///\n");
+    printf("///            de estoque, com finalidade didática e ilustrativos.         ///\n");
+    printf("///         Convém conter todos os módulos e funcionalidades exigidos     ///\n");
+    printf("///              pela disciplina para um controle de estoque mais           ///\n");
+    printf("///                         interativo e eficiênte.                        ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
