0a4912d2521e619e97a2f41958b2e346fbd30610
ZeClaudio-Jr <zeclaudio_jr5@yahoo.com.br>
2021-02-27 10:29:10 -0300

    Adição de Mód. Sobre

---
+++ moduloSobre.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "assin.h"
+
+
+
+void menuSobre(void) {
+
+    printf("\n");
+    printf("           Bem vindo ao seu programa de Controle de Estoque SIG-UFRN\n");
+    printf("\n");
+    printf("  ///////////////////////////////////////////////////////////////////////////////\n");
+    printf("  /*****************************************************************************/\n");
+    printf("  /*/                                                                         /*/\n");
+    printf("  /*/          UFRN - Universidade Federal do Rio Grande do Norte             /*/\n");
+    printf("  /*/             Centro de Ensino Superior do Serido - Caico                 /*/\n");
+    printf("  /*/               Departamento de Computacao e Tecnologia                   /*/\n");
+    printf("  /*/                   Disciplina DCT1106 * Programacao                      /*/\n");
+    printf("  /*/             SIG-Inventory: Sistema de Controle de Estoque               /*/\n");
+    printf("  /*/       Developed by: Annielly Ferreira de Sousa - Jan, 2021, and         /*/\n");
+    printf("  /*/                     Jose Claudio de Araujo Junior - Jan, 2021           /*/\n");
+    printf("  /*/                                                                         /*/\n");
+    printf("  /*****************************************************************************/\n");
+    printf("  ///////////////////////////////////////////////////////////////////////////////\n");
+    printf("  //===========================================================================//\n");
+    printf("  //=                                                                         =//\n");
+    printf("  //=                                                                         =//\n");
+    printf("  //=        = = SIG-Inventory: Sistema de Controle de Estoque = =            =//\n");
+    printf("  //=                                                                         =//\n");
+    printf("  //=   Programa desenvolvido em prol do desenvolvimento e aperfeicoamento    =//\n");
+    printf("  //=  da disciplina DCT1106 - Programacao. Tal trabalho pretende instituir   =//\n");
+    printf("  //=     um programa de controle de estoque, com finalidade didatica e       =//\n");
+    printf("  //=    ilustrativos. Convem conter todos os modulos e funcionalidades       =//\n");
+    printf("  //=       exigidos pela disciplina para um controle de estoque mais         =//\n");
+    printf("  //=    interativo e eficiente. Contera, alem do menu PRINCIPAL e SOBRE,     =//\n");
+    printf("  //=       outros menus em que os usuarios poderao adicionar produtos,       =//\n");
+    printf("  //=       alterar suas informacoes, assim como remove-los do estoque.       =//\n");
+    printf("  //=                                                                         =//\n");
+    printf("  //=                                                                         =//\n");
+    printf("  //= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =//\n");
+    printf("  ///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("                Digite ENTER para retornar ao MENU PRINCIPAL \n");
+    getchar();
+}
+
+
