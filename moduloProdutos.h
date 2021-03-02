12f6bc44e0057131aeb12d1f10dc5da752c5d7ab
ZeClaudio-Jr <zeclaudio_jr5@yahoo.com.br>
2021-03-02 10:16:26 -0300

    Adição moduloProdutos.h

---
+++ moduloProdutos.h
@@ -0,0 +1,10 @@
+//* Assinatura da função
+char menuProdutos(void); 
+
+//MODULO PRODUTO
+void navegacaoProduto(void); //Menu primário PRODUTOS
+
+void cadastrarProduto(void); //Menu secundário PRODUTOS
+void alterarProduto(void); //Menu secundário PRODUTOS
+void pesquisarProduto(void); //Menu secundário PRODUTOS
+void excluirProduto(void); //Menu secundário PRODUTOS
