2b2f823921554485b5fac76f9d2fe08279e02658
annielly <annielly@ufrn.edu.br>
2021-03-07 20:59:32 -0300

    bibliotecade validações

---
+++ validacao.h
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////////////////////////
+///             UFRN - Universidade Federal do Rio Grande do Norte          ///
+///                 Centro de Ensino Superior do Serido - Caico             ///
+///               Departamento de Computacao e Tecnologia                   ///
+///                  Disciplina DCT1106 * Programação                       ///
+///              SIG-Inventory: Sistema de Controle de Estoque              ///
+///         Developed by: Annielly Ferreira de Sousa - Jan, 2021, and       ///
+///                       Jose Claudio de Araujo Junior - Jan, 2021         ///
+///////////////////////////////////////////////////////////////////////////////
+
+
+//assinatura da função validaNome
+int validaNome(char nome[]);
+
+//assinatura da função validaCodigo
+int validaCodigo(char codigo[]);
+
+//assinatura da função validaDescricao
+int validaDescricao(char desc[]);
