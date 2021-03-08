dc5195ea619eaa0e0f6f52f8eb7e0948d6d61ffa
annielly <annielly@ufrn.edu.br>
2021-03-07 21:07:27 -0300

    funçoes de validação

---
+++ validacao.c
@@ -0,0 +1,39 @@
+#include <string.h>
+
+// função validaNome
+int validaNome(char nome[]){
+
+  for(int i = 0; i < strlen(nome); i++){
+    if((nome[i] >= 0 && nome[i] <= 64) || (nome[i] >= 122 && nome[i] <= 97)){ //se estiver nessa faixa não são letras   
+      return 0;
+    }else  if (strlen(nome) > 24){ // limita numero de caracteres digitados
+      return 0;
+    }
+  }
+  return 1;
+}
+
+// função validaCodigo
+int validaCodigo(char codigo[]){
+
+  for(int i = 0; i < strlen(codigo); i++){
+    if((codigo[i] >= 0 && codigo[i] <= 47) || (codigo[i] >= 58 && codigo[i] <= 126)){    return 0;
+    }else  if (strlen(codigo) > 4){ // limita numero de caracteres digitados
+      return 0;
+    }
+  }
+  return 1;
+}
+
+// função validaDescricao
+int validaDescricao(char desc[]){
+
+  for(int i = 0; i < strlen(desc); i++){
+    if((desc[i] >= 0 && desc[i] <= 64) || (desc[i] >= 122 && desc[i] <= 97)){    
+        return 0;
+    }else  if (strlen(desc) > 49){ // limita numero de caracteres digitados
+      return 0;
+    }
+  }
+  return 1;
+}
