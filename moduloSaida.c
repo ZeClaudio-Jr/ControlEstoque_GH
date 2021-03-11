d4ad0957a18a779211ab7cddafdaa1d090fb87d4
annielly <annielly@ufrn.edu.br>
2021-03-11 15:46:44 -0300

    add entradas do excluir

--- moduloSaida.c
+++ moduloSaida.c
@@ -71,7 +71,7 @@ char menuSaida(void){
     printf("  |*|                                                                        |*|\n");
     printf("  >>>-##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##-<<<\n");
     printf("\n");
-    printf("Digite a opcao desejada: ");
+    printf("\tDigite a opcao desejada: ");
     scanf("%c", &op);
     getchar();
     return op;
@@ -396,6 +396,7 @@ void pesquisarSaida(void){
 void excluirSaida(void){
   char nome [25];
   char forn[25];
+  int validar;
     system("cls");
     printf("\n");
     printf("  *#--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--##--#*\n");
@@ -418,12 +419,29 @@ void excluirSaida(void){
     printf("  \n");
     printf("  >>>>>>>>     Informe o nome do produto ou o nome do fornecedor:     <<<<<<<<  \n");
     printf("  \n");
-    printf("  |*|           **         Nome do produto: ");
-    scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", nome);
-    getchar();
-    printf("  *|*           **         Nome do ornecedor: ");
-    scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", forn);
-	  getchar();
+    do{
+      printf("  |*|          **        Nome do produto: ");
+      scanf("%s", nome);
+      getchar();
+      validar = validaNome(nome);
+      if(validar == 1){
+            printf("  |*|          **        Descricao valida!\n");
+      }else{
+            printf("  |*|          **        Descricao invalida!\n");
+      }
+    }while(validar != 1);
+    printf("  \n");    
+    do{
+      printf("  |*|          **        Fornecedor: ");
+      scanf("%s", forn);
+      getchar();
+      validar = validaForne(forn);
+        if(validar == 1){
+          printf("  |*|          **        Fornecedor valido!\n");
+        }else{
+          printf("  |*|          **        Fornecedor invalido!\n");
+        }
+    }while(validar != 1);   
     printf("  \n");
     printf("  \n");
     printf("  \n");
