341bf24eb513627174ab040d0e48ef344f36b571
ZeClaudio-Jr <zeclaudio_jr5@yahoo.com.br>
2021-03-11 22:21:48 -0300

    Adição de assinaturas mod.Produtos

--- assin.h
+++ assin.h
@@ -1,18 +1,18 @@
 //* Assinatura das funções
 char menuPrincipal(void);
 void menuSobre(void);
-// char menuProdutos(void);            
+char menuProdutos(void);            
 char menuEntrada(void);      
 char menuSaida(void);
 void menuRelatorio(void); 
 
-// //MODULO PRODUTO
-// void navegacaoProduto(void); //Menu primário PRODUTOS
+//MODULO PRODUTO
+void navegacaoProduto(void); //Menu primário PRODUTOS
 
-// void cadastrarProduto(void); //Menu secundário PRODUTOS
-// void alterarProduto(void); //Menu secundário PRODUTOS
-// void pesquisarProduto(void); //Menu secundário PRODUTOS
-// void excluirProduto(void); //Menu secundário PRODUTOS
+void cadastrarProduto(void); //Menu secundário PRODUTOS
+void alterarProduto(void); //Menu secundário PRODUTOS
+void pesquisarProduto(void); //Menu secundário PRODUTOS
+void excluirProduto(void); //Menu secundário PRODUTOS
 
 //MODULO ENTRADA
 void navegacaoEntrada(void);
