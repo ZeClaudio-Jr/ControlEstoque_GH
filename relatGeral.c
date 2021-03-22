9d424e874a8b43d25f0e1faf88fcf845e887b023
ZeClaudio-Jr <zeclaudio_jr5@yahoo.com.br>
2021-03-22 10:26:14 -0300

    Ajustes

--- relatGeral.c
+++ relatGeral.c
@@ -41,6 +41,8 @@ void relatorioGeral(void) {
     printf("      >>    CODIGO: 0129\n");
     printf("      >>    DESCRICAO.: Sandalia unissex borracha produzida pela alpargatas\n");
     printf("      >>    FORNECEDOR: Alpargatas\n");
+    printf("      >>    ENTRADA: 450 UNID\n");
+    printf("      >>    SAIDA: 363 UNID\n");
     printf("      >>    DATA COMPRA: 26/01/2021\n");
     printf("      >>    DATA VALIDADE: 20/04/2023\n");
     printf("      >>    ESTOQUE: 87 UNID\n"); 
@@ -52,6 +54,8 @@ void relatorioGeral(void) {
     printf("      >>    CODIGO: 5391\n");
     printf("      >>    DESCRICAO.: Leite desnatado em po que contem um mix exclusivo de vitaminas\n");
     printf("      >>    FORNECEDOR: Nestle\n");
+    printf("      >>    ENTRADA: 500 UNID\n");
+    printf("      >>    SAIDA: 363 UNID\n");
     printf("      >>    DATA COMPRA: 09/02/2021\n");
     printf("      >>    DATA VALIDADE: 21/12/2022\n");
     printf("      >>    ESTOQUE: 137 UNID\n");
@@ -63,6 +67,8 @@ void relatorioGeral(void) {
     printf("      >>    CODIGO: 1700\n");
     printf("      >>    DESCRICAO.: Palitos com tamanho diferenciado, alongado e seguro\n");
     printf("      >>    FORNECEDOR: Fiat Lux\n");
+    printf("      >>    ENTRADA: 200 UNID\n");
+    printf("      >>    SAIDA: 161 UNID\n");
     printf("      >>    DATA COMPRA: 12/11/2020\n");
     printf("      >>    DATA VALIDADE: 20/08/2025\n");
     printf("      >>    ESTOQUE: 39 UNID\n");
@@ -74,6 +80,8 @@ void relatorioGeral(void) {
     printf("      >>    CODIGO: 0360\n");
     printf("      >>    DESCRICAO.: Esponja de poliuretano, bactericida, fibra sintetica com material abrasivo\n");
     printf("      >>    FORNECEDOR: Bombril\n");
+    printf("      >>    ENTRADA: 300 UNID\n");
+    printf("      >>    SAIDA: 199 UNID\n");
     printf("      >>    DATA COMPRA: 30/01/2021\n");
     printf("      >>    DATA VALIDADE: 17/03/2024\n");
     printf("      >>    ESTOQUE: 101 UNID\n");
