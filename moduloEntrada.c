b96dbc96086843d4a67e51628e317f5a27745a77
annielly <annielly@ufrn.edu.br>
2021-04-06 18:14:04 -0300

    struct

--- moduloEntrada.c
+++ moduloEntrada.c
@@ -7,17 +7,17 @@
 
 
 //Definiçãp da struct Entrada
-typedef struct entrada Entrada;
+// typedef struct entrada Entrada;
 
-struct entrada{
-  char nome [25];
-  char codigo [5];  
-  char desc[50];
-  char forn[25];
-  char qtde[5];
-  int dd, mm, aaaa;
-  int validar;
-};
+// struct entrada{
+//   char nome [25];
+//   char codigo [5];  
+//   char desc[50];
+//   char forn[25];
+//   char qtde[5];
+//   int dd, mm, aaaa;
+//   int validar;
+// };
 
 
 //MODULO ENTRADA
