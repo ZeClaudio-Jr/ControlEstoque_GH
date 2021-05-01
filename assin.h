ee51a54b801f6f09bfc14558fe174ec4b50f4997
ZeClaudio-Jr <zeclaudio_jr5@yahoo.com.br>
2021-05-01 16:40:56 -0300

    Retirada Bibliot. Produtos

--- assin.h
+++ assin.h
@@ -6,47 +6,10 @@
 //* Assinatura das funções
 char menuPrincipal(void);
 void menuSobre(void);
-char menuProdutos(void);
 char menuEntrada(void);    
 char menuSaida(void);
 char menuRelatorio(void);
 
-
-//Definição da estrutura(struct) Produtos
-typedef struct produtos Produtos;
-struct produtos{
-  char nome [71];
-  char codigo [5];
-  float valor;
-  int dd, mm, aaaa; //???? Será que é char?
-  int status;
-};
-
-
-//MODULO PRODUTO
-
-void navegacaoProduto(void); //Menu primário PRODUTOS
-
-void cadastrarPrincipal(void);
-void pesquisarPrincipal(void);
-void alterarPrincipal(void);
-void excluirPrincipal(void);
-
-
-Produtos* cadastrarProduto(void); //Menu secundário PRODUTOS
-char* pesquisarProduto(void); //Menu secundário PRODUTOS
-char* alterarProduto(void); //Menu secundário PRODUTOS
-char* excluirProduto(void); //Menu secundário PRODUTOS
-
-
-void gravarProduto(Produtos*);
-void exibirProduto(Produtos*);
-Produtos* buscarProduto(char*);
-void regravarProduto(Produtos*);
-
-
-//MODULO ENTRADA
-typedef struct entrada Entrada; //Definição do tipo Entrada
 struct entrada{ //STRUCT para armazenar os dados da entrada
   char nome [25];
   char codigo [5];  
