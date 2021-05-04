e75fdd0c37f817a71ef65dce61074e34641a27eb
ZeClaudio-Jr <zeclaudio_jr5@yahoo.com.br>
2021-05-03 22:57:17 -0300

    Adição relatorios.h

---
+++ relatorios.h
@@ -0,0 +1,13 @@
+// MODULO RELATÓRIO
+void navegacaoRelatorio(void);
+
+char menuRelatorio(void); //Menu primário RELATÓRIO
+
+void relatorioGeral(void);
+void relatorioProdutos(void);
+
+void telaRelatProdEspecifico(void);
+void telaRelatProdutos(char*);
+
+void listProdutosPorCodigo(char*);
+void listGeralProdutos(char*);
